cef37393fb7b7f207d5e4157a5e14ec93bdb1093
aluno <paulinhop147@gmail.com>
2018-11-01 17:17:14 -0300

    dsfsdfsdfsdfsdfs

--- app/src/main/java/banco_dados/dadosViaGet.java
+++ app/src/main/java/banco_dados/dadosViaGet.java
@@ -42,7 +42,7 @@ import fonte_controle.Noticia;
 
 import static android.content.ContentValues.TAG;
 public class dadosViaGet {
-    private String ipBanco="http://pi.rbeninca.com.br/html/";
+    private String ipBanco="http://pi.rbeninca.com.br/";
     private ArrayList<Fonte> fontes=new ArrayList<Fonte>();
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
 
